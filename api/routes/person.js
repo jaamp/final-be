78f04535d28756e75e07a1e287a559876744ea9a
jaamp <jt.projectclass@gmail.com>
2019-08-30 19:32:25 -0700

    adding comments

--- api/routes/person.js
+++ api/routes/person.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 
 router.get('/', async (req, res, next) => {
     const status = 200
+    //API routes to be used for views
     //const response = await Person.find({ "assignment.graded": {$eq:true}}).select('fname lname email assignment')
     //const response = await Person.find({ "assignment.graded": {$eq:false}}).select('fname lname email assignment')
     //const response = await Person.find({ role: {$eq:"student"}}).select('fname lname email assignment')
